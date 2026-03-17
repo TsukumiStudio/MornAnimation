6ec39f3420db6b7b7670bdfc09c59e49ba04ae2c
matsufriends <pikapikan0526@gmail.com>
2026-03-17 12:37:49 +0900

    MornAnimationBase: UnityEvent用のShow()/Hide()を追加
    引数なしでShowAsync/HideAsyncをForget実行するラッパー。
    UnityEventからMornAnimationBaseのShow/Hideを設定可能に。

--- Animation/MornAnimationBase.cs
+++ Animation/MornAnimationBase.cs
@@ -9,5 +9,11 @@ namespace MornLib
 		public abstract UniTask ShowAsync(CancellationToken ct = default);
 		public abstract UniTask HideAsync(CancellationToken ct = default);
 		public abstract void DebugInitialize();
+
+		/// <summary>UnityEvent用。引数なしでShowAsyncを実行する。</summary>
+		public void Show() => ShowAsync().Forget();
+
+		/// <summary>UnityEvent用。引数なしでHideAsyncを実行する。</summary>
+		public void Hide() => HideAsync().Forget();
 	}
 }
