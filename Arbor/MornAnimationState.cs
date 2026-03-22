cad7b94d12cdaf7424c06b071b625925b38f6be9
matsufriends <pikapikan0526@gmail.com>
2026-03-22 16:52:40 +0900

    MornAnimationState: Undo対応 + interval/delay/isWaitEach追加
    - CollectFromChildren: RecordUndo追加
    - interval: 各target間の遅延
    - delay: 全体開始前の遅延
    - isWaitEach: 各targetの完了を待ってから次へ

--- Arbor/MornAnimationState.cs
+++ Arbor/MornAnimationState.cs
@@ -11,16 +11,35 @@ namespace MornLib
 		[SerializeField] private List<MornAnimationEntry> _targets;
 		[SerializeField] private StateLink _onComplete;
 		[SerializeField] private bool _isExecuteAsIsolated;
+		[SerializeField] private float _interval;
+		[SerializeField] private float _delay;
+		[SerializeField] private bool _isWaitEach;
 
 		public override async void OnStateBegin()
 		{
 			var ct = _isExecuteAsIsolated ? destroyCancellationToken : CancellationTokenOnEnd;
 			try
 			{
+				if (_delay > 0f)
+				{
+					await MornAnimationUtil.WaitSeconds(_delay, ct);
+				}
+
 				var tasks = new List<UniTask>();
 				foreach (var target in _targets)
 				{
 					tasks.Add(target.ExecuteAsync(ct));
+
+					if (_isWaitEach)
+					{
+						await UniTask.WhenAll(tasks);
+						tasks.Clear();
+					}
+
+					if (_interval > 0f)
+					{
+						await MornAnimationUtil.WaitSeconds(_interval, ct);
+					}
 				}
 
 				await UniTask.WhenAll(tasks);
@@ -45,6 +64,7 @@ namespace MornLib
 
 		private void CollectFromChildren(bool toShow)
 		{
+			MornAnimationUtil.RecordUndo(this, "CollectFromChildren");
 			var anims = MornAnimationUtil.CollectChildAnimations(this);
 			_targets = new List<MornAnimationEntry>();
 			foreach (var anim in anims)
